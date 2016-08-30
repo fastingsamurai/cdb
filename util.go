e843b57fadb751324585ea13b3746a89c8318c8f
Will <will@ed.com>
2016-08-30 15:57:11 -0400

    don't use defer for performance

--- util.go
+++ util.go
@@ -7,23 +7,24 @@ import (
 
 func (cdb *CDB) readTuple(r io.ReaderAt, offset uint32) (uint32, uint32, error) {
 	tuple := cdb.tuplestorage.Get().([]byte)
-	defer cdb.tuplestorage.Put(tuple)
 	_, err := r.ReadAt(tuple, int64(offset))
 	if err != nil {
+		cdb.tuplestorage.Put(tuple)
 		return 0, 0, err
 	}
 
 	first := binary.LittleEndian.Uint32(tuple[:4])
 	second := binary.LittleEndian.Uint32(tuple[4:])
+	cdb.tuplestorage.Put(tuple)
 	return first, second, nil
 }
 
 func (cdb *Writer) writeTuple(w io.Writer, first, second uint32) error {
 	tuple := cdb.tuplestorage.Get().([]byte)
-	defer cdb.tuplestorage.Put(tuple)
 	binary.LittleEndian.PutUint32(tuple[:4], first)
 	binary.LittleEndian.PutUint32(tuple[4:], second)
 
 	_, err := w.Write(tuple)
+	cdb.tuplestorage.Put(tuple)
 	return err
 }
